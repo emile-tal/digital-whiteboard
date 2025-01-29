cebb51a6e7fdfdd293a5a03cc0532a7903c37b5f
Emile <emiletal@gmail.com>
2025-01-29 20:01:32 +0100

    Hardcode AI server

---
+++ backend.ts
@@ -0,0 +1,9 @@
+export function backendCall(mode: string) {
+    if (mode === 'write') {
+        return ['write', "This is a hardcoded mock of an AI backend writing something on the whiteboard."]
+    } else if (mode === 'append') {
+        return ['append', 'Appending text to the whiteboard.']
+    } else if (mode === 'annotate') {
+        return ['annotate', /whiteboard/]
+    }
+}
